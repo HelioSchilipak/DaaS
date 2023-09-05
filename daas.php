4e0dce6a03c4f21dab5b06a6f0414bdfa4ecb251
HelioSchilipak <42068577+HelioSchilipak@users.noreply.github.com>
2023-09-05 08:42:07 -0300

    Add files via upload

---
+++ daas.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="shortcut icon" href="./img/faviconmain.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DaaS - Coat Team</title>
+    <link rel="stylesheet" href=".\cssdaas\daas.css">
+</head>
+
+<body>
+    <div class="container center">
+
+<img src="\imgdaas\daas.png" alt="">
+<div class=titulo>
+    <h1>
+    <p>Troubleshooting Implantação 2023<p>
+    </h1>
+
+</div>
+
+
+<p><br><br></p>
+<p></p>
+<p></p>
+<p>   
+    <a href="https://daas.coat.team/arquivos/windows6.1-kb4474419-v3-x64_b5614c6cea5cb4e198717789633dca16308ef79c.msu" download="">(04.09.2023) windows6.1-kb4474419-v3-x64.msu</a>
+    atualização Win7 x64 para instalação NET Framework
+</p>
+
+<p>
+    <a href="https://daas.coat.team/arquivos/windows6.1-kb4474419-v3-x86_0f687d50402790f340087c576886501b3223bec6.msu" download="">(04.09.2023) windows6.1-kb4474419-v3-x86.msu</a>
+    atualização Win7 x32 para instalação NET Framework
+</p>   
+
+<p>
+    <a href="https://daas.coat.team/arquivos/Aluno_ADM.bat" download="">(28.08.2023) Adiciona aluno admin</a>
+     .bat adiciona permissão de admin para instalação arlequim/citrix
+</p>
+ 
+<p>
+    <a href="https://daas.coat.team/arquivos/Aluno_Users.bat" download="">(28.08.2023) Remove aluno admin</a>
+     .bat remove permissão de admin para instalação arlequim/citrix
+</p>
+
+<p>
+    <a href="https://daas.coat.team/arquivos/Treinamento Daas Arlequim - final.pdf" download="">(16.08.2023) Treinamento Daas Arlequim</a>
+     desenvolvido e ministrado por Douglas Arceno e Magdiel Pereira
+</p>
+
+
+</body>
+</html>
